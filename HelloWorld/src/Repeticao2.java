d6f72fd8e2b098e8446c30f012aac76955aedebb
l2j_git <paulomsdos@gmail.com>
2025-01-18 20:38:04 +0000

    Just an exercise

---
+++ HelloWorld/src/Repeticao2.java
@@ -0,0 +1,9 @@
+public class Repeticao2 {
+        public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Numero: " + i);
+            i++; // Vai adicionando ao contador
+        }
+    }
+}
